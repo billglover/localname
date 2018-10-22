0553ea4ffbd2fd116a2a222fa3c2e7c660ff61d1
Bill Glover <bill@billglover.co.uk>
2018-10-22 23:35:35 +0100

    Move server application to cmd folder

---
+++ cmd/server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("environment variable PORT is required")
+	}
+
+	http.HandleFunc("/ip", handler)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func handler(rw http.ResponseWriter, req *http.Request) {
+	xff := req.Header.Get("X-Forwarded-For")
+	ip := net.ParseIP(xff)
+
+	if ip == nil {
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintln(rw, ip)
+}
